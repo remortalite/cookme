5be3d8c882fb021b1e104b5e5ca32d2572284e33
Rem M. <morrig407@gmail.com>
2024-11-20 17:17:53 +0700

    Add python -dotenv package; add SECRET_KEY import from .dotenv

--- cookme/cookme/settings.py
+++ cookme/cookme/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ua6s7cd&q2ho5eyy_vcv=gq7rkhvfy^%)$n!=ak-%mx0tmd=fs'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    'django-insecure-ua6s7cd&q2ho5eyy_vcv=gq7rkhvfy^%)$n!=ak-%mx0tmd=fs')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
